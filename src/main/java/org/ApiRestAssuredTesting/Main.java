df6273ff99f77daf51e28c4952390c4db96b2ca7
poojasingh1406 <poojasingh1406@gmail.com>
2025-02-18 12:05:23 +0000

    Commiting main class after update

--- src/main/java/org/ApiRestAssuredTesting/Main.java
+++ src/main/java/org/ApiRestAssuredTesting/Main.java
@@ -2,6 +2,6 @@ package org.ApiRestAssuredTesting;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world1!");
     }
 }
